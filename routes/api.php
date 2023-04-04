97aba0abc017a79cb16f7a4dc31e90a2c21e1c90
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2023-04-05 00:23:18 +0400

    create user and admin auth routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Auth\PasswordResetController;
+use App\Http\Controllers\Api\AdminController;
+use App\Http\Controllers\Api\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +16,37 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::prefix('v1')->group(function () {
+    Route::controller(AuthController::class)->group(function(){
+        Route::post('/admin/create', 'register');
+        Route::post('/admin/login',  'login');
+        Route::post('/user/create', 'register');
+        Route::post('/user/login',  'login');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+        Route::middleware(['auth'])->group(function () {
+            Route::post('/user/logout', 'logout');
+            Route::post('/admin/logout', 'logout');
+        });
+    });
+
+    Route::controller(PasswordResetController::class)->group(function(){
+        Route::post('/user/forgot-password', 'forgot_password');
+        Route::post('/user/reset-password-tokens',  'password_reset');
+    });
+
+    Route::middleware(['jwt'])->group(function () {
+        Route::controller(AdminController::class)->prefix('/admin')->name('admin.')->group(function(){
+            Route::get('/user-listing', 'userList')->name('user-list');
+            Route::put('/user-edit/{uuid}', 'userEdit')->name('user-edit');
+            Route::delete('/user-delete/{uuid}', 'userDelete')->name('user-delete');
+        });
+
+        Route::controller(UserController::class)->prefix('/user')->name('user.')->group(function(){
+            Route::get('/', 'get');
+            Route::get('/orders', 'orders')->name('orders');
+            Route::put('/edit', 'edit')->name('edit');
+            Route::delete('/delete', 'delete')->name('delete');
+        });
+    });
 });
+
